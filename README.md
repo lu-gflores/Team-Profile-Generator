9186400199f30737c85b2e8baeeec73cb9916c2b
George Flores <georgeflores886@gmail.com>
2020-05-29 09:20:26 -0400

    fixed inquirer prompt, starting html rendering

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Team-Profile-Generator
+
+## Description 
+
+This project involves generating software engineering team profiles using Node CLI. This prompts the user to input information about the team manager and team members, where they can input any number of members as well as mix the team with both interns and engineers. Once the team has been built, the applicaiton will create an HTML file that will display the roster based on the information provided. 
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Tests](#tests)
+* [Author](#author)
+
+## Installation
+Before installing this project, Node.js must be installed to run CLI.
+
+Install the project using the following command: 
+```javascript
+
+npm i inquirer
+
+```
+
+## Usage
+
+Once installed, run ```node app.js``` through the terminal to run the project:
+
+
+
--- app.js
+++ app.js
@@ -10,44 +10,170 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+//contains employee objects
+const teamArray = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-inquirer
-.prompt([
-    {
-        type: 'input',
-        message: "What is your name?",
-        name: 'name',
-    }, {
-        type: 'list',
-        message: 'What is your current role?',
-        name: 'role',
-        choices: ['Manager', 'Engineer', 'Intern']
-    }, 
-    {
-        type: 'input',
-        message: 'WHat is your work ID?',
-        name: 'id'
 
-    }, {
-        type: 'input',
-        message: 'Enter your email: ',
-        name: 'email',
-    } , {
-        type:'input',
-        message: 'Enter your GitHub username: ',
-        name: 'github'
-    } 
-]).then (function(name, role, workID, email, github) {
-    if(role === 'Manager') {
-        
-    }
-});
+const mainPrompt = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: "Enter team member's role: ",
+            name: 'role',
+            choices: ['Manager', 'Engineer', 'Intern']
+        }
+    ]).then(function (answer) {
+        if (answer.role === 'Manager') {
+            inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        message: "Enter a team member's name: ",
+                        name: 'name',
+                    },{
+                        type: 'input',
+                        message: "Enter team member's ID: ",
+                        name: 'id'
+            
+                    }, {
+                        type: 'input',
+                        message: "Enter team member's email: ",
+                        name: 'email',
+                    }, {
+                        type: 'input',
+                        message: 'Enter your office number: ',
+                        name: 'officeNumber',
+                    }
+                ]).then(function (data) {
+                    const currManager = new Manager(data.name, data.id, data.email, data.officeNumber)
+                    console.log(currManager);
+                    teamArray.push(currManager);
+                    addMoreMembers();
+                })
+
+        } else if (answer.role === 'Engineer') {
+            inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        message: "Enter a team member's name: ",
+                        name: 'name',
+                    },{
+                        type: 'input',
+                        message: "Enter team member's ID: ",
+                        name: 'id'
+            
+                    }, {
+                        type: 'input',
+                        message: "Enter team member's email: ",
+                        name: 'email',
+                    }, {
+                        type: 'input',
+                        message: 'Enter your github username: ',
+                        name: 'github',
+                    }
+                ]).then(function (data) {
+                    const currEngineer = new Engineer(data.name, data.id, data.email, data.github)
+                    console.log(currEngineer);
+                    teamArray.push(currEngineer);
+                    addMoreMembers();
+                })
+
+        } else {
+            inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        message: "Enter a team member's name: ",
+                        name: 'name',
+                    },{
+                        type: 'input',
+                        message: "Enter team member's ID: ",
+                        name: 'id'
+            
+                    }, {
+                        type: 'input',
+                        message: "Enter team member's email: ",
+                        name: 'email',
+                    }, {
+                        type: 'input',
+                        message: 'Enter your school name: ',
+                        name: 'school',
+                    }
+                ]).then(function (data) {
+                    const currIntern = new Intern(data.name, data.id, data.email, data.school)
+                    console.log(currIntern);
+                    teamArray.push(currIntern);
+                    addMoreMembers();
+                })
+        }
+
+    });
+
+}
+mainPrompt();
+
+const addMoreMembers = () => {
+    inquirer.prompt([
+        {
+            type:'confirm',
+            message: 'Add more members?',
+            name: 'addMember'
+        }
+    ]).then(function(answer) {
+            if(answer.addMember === true) {
+                mainPrompt();
+            }
+            else {
+                renderHTML();
+            }
+    }) 
+}
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-const teamArray = []
+
+
+const renderHTML = () => {
+    const header = `
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+            <title>My Team</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <link rel="stylesheet" href="style.css">
+            <script src="https://kit.fontawesome.com/c502137733.js"></script>
+        </head>
+
+        <body>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 jumbotron mb-3 team-heading">
+                        <h1 class="text-center">My Team</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="team-area col-12 d-flex justify-content-center">
+                        {{ team }}
+                    </div>
+                </div>
+            </div>
+        </body>
+
+        </html>
+        `
+
+}
 
 
 // After you have your html, you're now ready to create an HTML file using the HTML
